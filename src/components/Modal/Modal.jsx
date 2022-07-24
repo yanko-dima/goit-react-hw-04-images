0c15d881a2c429d4afc442979a28773ff0adc7a3
Dmytro Yanko <yanko.dmitriy@gmail.com>
2022-07-24 22:52:08 +0300

    work

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,34 @@
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 
-const Modal = ({ largeImageURL, searchKey }) => {
-  return (
-    <div class={css.Overlay}>
-      <div class={css.Modal}>
-        <img src={largeImageURL} alt={searchKey} />
-      </div>
-    </div>
-  );
-};
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  componentDidMount() {
+    console.log('Modal componentDidMount');
+    const { onClose } = this.props;
+
+    window.addEventListener('keydown', e => {
+      if (e.code === 'Escape') {
+        console.log('Нужно закрыть модалку');
+        onClose();
+      }
+    });
+  }
+
+  render() {
+    const { largeImageURL, searchKey } = this.props;
+
+    return createPortal(
+      <div className={css.Overlay}>
+        <div className={css.Modal}>
+          <img src={largeImageURL} alt={searchKey} />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
 
 export default Modal;
